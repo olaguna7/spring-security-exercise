d977c7b12309f65ddad02a7f062c902628eeae97
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-29 18:26:39 +0200

    First commit

---
+++ src/test/java/com/oscar/springsecurityexercise/SpringSecurityExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscar.springsecurityexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
